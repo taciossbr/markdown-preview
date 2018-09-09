83c095cdc99aa3b31769ccaa28087cf1e609c1b4
taciossbr <tacioss.br@gmail.com>
2018-09-09 20:44:25 -0300

    Ok

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
+  // mode: 'production',
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: 'dist/',
     filename: 'build.js'
   },
   module: {
